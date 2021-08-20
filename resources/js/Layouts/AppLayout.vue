f60b6be309102061feee7a8ac3099cec69b8f922
Erick Mercedes <erickmercedesemc@gmail.com>
2021-08-21 05:12:20 +0800

    Creacion de la vista nota index

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -22,6 +22,9 @@
                                 <jet-nav-link :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </jet-nav-link>
+                                <jet-nav-link :href="route('notes.index')" :active="route().current('notes.*')">
+                                    Notes
+                                </jet-nav-link>                                
                             </div>
                         </div>
 
@@ -145,6 +148,9 @@
                         <jet-responsive-nav-link :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
                         </jet-responsive-nav-link>
+                        <jet-nav-link :href="route('notes.index')" :active="route().current('notes.*')">
+                            Notes
+                        </jet-nav-link>
                     </div>
 
                     <!-- Responsive Settings Options -->
